077cefeda9189c93b1c43fe44fae9ece164b86e9
Wimsb <radmirradmir324@gmail.com>
2025-11-05 17:26:24 +0500

    Добавьте файлы проекта.

---
+++ Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Murzayanov41
+{
+    internal class Manager
+    {
+        static public Frame MainFrame { get; set; }
+    }
+}
